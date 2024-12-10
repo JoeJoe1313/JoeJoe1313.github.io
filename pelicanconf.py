2418a77f1f2de6d8841e002d57377b3782bf8f0c
JoeJoe1313 <levcheva_joana@yahoo.com>
2024-12-10 17:41:37 +0200

    add gitignore

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,16 +31,17 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
+    ("GitHub", "https://github.com/JoeJoe1313"),
+    # ("Pelican", "https://getpelican.com/"),
+    # ("Python.org", "https://www.python.org/"),
+    # ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    # ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("LinkedIn", "https://www.linkedin.com/in/joana-levtcheva-479844164/"),
+    # ("Another social link", "#"),
 )
 
 DEFAULT_PAGINATION = 10
