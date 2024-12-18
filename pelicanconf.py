cea7b8677e842cd8877b16540d3c199742ff8825
JoeJoe1313 <levcheva_joana@yahoo.com>
2024-12-18 22:20:25 +0200

    change back to linebreak_automatic

--- pelicanconf.py
+++ pelicanconf.py
@@ -60,7 +60,7 @@ PLUGINS = [
 SUMMARY_USE_FIRST_PARAGRAPH = True
 WITH_FUTURE_DATES = False
 MATH_JAX = {
-    "responsive": True,
+    "linebreak_automatic": True,
 }
 
 # Uncomment following line if you want document-relative URLs when developing
